c0dc8276be0fb879fffccd7c1ea68698511b33ab
nanayah99 <nanayah99@naver.com>
2024-11-19 18:14:42 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1496 ms, Memory: 88984 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int numberParticipants = Integer.parseInt(br.readLine());
+
+        int[][] participantsInfo= seekInformationSubscribedUsers(numberParticipants, br);
+
+//        Arrays.sort(participantsInfo, ((o1, o2) -> {
+//            return o1[0] - o2[0];
+//        }));
+
+        Arrays.sort(participantsInfo, ((o1, o2) -> {
+            int move = o1[1] - o2[1];
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return move;
+        }));
+
+        for (int i = 0; i < numberParticipants; i++) {
+            System.out.println(participantsInfo[i][0] + " " + participantsInfo[i][1]);
+        }
+    }
+
+    private static int[][] seekInformationSubscribedUsers(int numberParticipants, BufferedReader br) throws Exception{
+        int[][] participantsInfo = new int[numberParticipants][2];
+        int[] participantInfo;
+        for (int i = 0; i < numberParticipants; i++) {
+            participantInfo = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            participantsInfo[i][0] = participantInfo[0];
+            participantsInfo[i][1] = participantInfo[1];
+        }
+
+        return participantsInfo;
+    }
+}
