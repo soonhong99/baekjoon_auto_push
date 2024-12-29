93142fb8b3c111567d4070c25ebe4d1ae029c10f
nanayah99 <nanayah99@naver.com>
2024-12-29 13:06:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 264 ms, Memory: 38036 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 
@@ -5,39 +6,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        
-        int start = input[0];
-        int target = input[1];
-        
-        System.out.println(findMinimumTime(start, target));
-    }
-    
-    private static int findMinimumTime(int start, int target) {
-        Queue<Integer> queue = new LinkedList<>();
-        Map<Integer, Integer> visited = new HashMap<>();  // 위치, 시간
-        
-        queue.offer(start);
-        visited.put(start, 0);
-        
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            int time = visited.get(current);
-            
-            if (current == target) {
-                return time;
-            }
-            
-            // 가능한 다음 위치들
-            int[] next = {current - 1, current + 1, current * 2};
-            
-            for (int nextPos : next) {
-                if (nextPos >= 0 && nextPos <= 100000 && !visited.containsKey(nextPos)) {
-                    queue.offer(nextPos);
-                    visited.put(nextPos, time + 1);
+        int currentLocation = input[0];
+        int findLocation = input[1];
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(currentLocation);
+        HashMap<Integer, Integer> depth = new HashMap<>();
+        depth.put(currentLocation, 0);
+
+        while (queue.size() != 0) {
+            int thisLocation= queue.removeFirst();
+            int thisDepth = depth.get(thisLocation);
+            thisDepth++;
+
+            int[] canGoList = {thisLocation - 1, thisLocation + 1, thisLocation * 2};
+
+            for (int i : canGoList) {
+                if (!depth.containsKey(i) && i >= 0 && i <= 100000) {
+                    depth.put(i, thisDepth);
+                    queue.add(i);
                 }
             }
+
+            if (depth.containsKey(findLocation))
+                break;
         }
-        
-        return -1;
+
+        System.out.println(depth.get(findLocation));
     }
 }
