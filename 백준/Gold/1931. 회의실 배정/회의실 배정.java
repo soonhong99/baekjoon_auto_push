deda12b5d46b664770cc8dad0b47aba0337248d1
nanayah99 <nanayah99@naver.com>
2025-01-05 20:56:43 +0900

    [Gold V] Title: 회의실 배정, Time: 512 ms, Memory: 46200 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,59 +2,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int conferenceNum = Integer.parseInt(br.readLine());
-
-        /*
-        첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다.
-        둘째 줄부터 N+1 줄까지 각 회의의 정보가 주어지는데 이것은 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다.
-        시작 시간과 끝나는 시간은 2 ** 31 - 1 보다 작거나 같은 자연수 또는 0이다.
-        첫째 줄에 최대 사용할 수 있는 회의의 최대 개수를 출력한다.
-
-        최대값? dp? 하나의 선택을 했을 때, 해당 선택이 뒤에 있는 결과에 영향을 미친다. -> 이게 sort가 되어 있어야 하는데, sort를 시키기 애매하네?
-        정렬의 필요성? -> 길이가 짧은 것 부터 이용한다? -> 정렬을 한 다음에 어떻게 선별할거야? -> 첫 시작점을 기준으로 정렬?
-        하나를 선택하고, 그 다음 선택지가 바로 바로 보일 수 있어야 한다.
-         */
-        List<int[]> conferenceSchedule = new ArrayList<>();
-
+        ArrayList<int[]> conferenceList = new ArrayList<>();
         for (int i = 0; i < conferenceNum; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int startNum = Integer.parseInt(st.nextToken());
-            int endNum = Integer.parseInt(st.nextToken());
-            conferenceSchedule.add(new int[] {startNum, endNum});
+            int startTime = Integer.parseInt(st.nextToken());
+            int endTime = Integer.parseInt(st.nextToken());
+            conferenceList.add(new int[] {startTime, endTime});
         }
-
-        Collections.sort(conferenceSchedule, (sc1, sc2) -> {
-            if (sc1[1] == sc2[1])
-                return sc1[0] - sc2[0];
-            return sc1[1] - sc2[1];
+        Collections.sort(conferenceList, (con1, con2) -> {
+            if (con2[1] == con1[1])
+                return con1[0] - con2[0];
+            return con1[1] - con2[1];
         });
-
-        int totalConferenceNum = 1;
-        int endTime = conferenceSchedule.get(0)[1];
-
+        int totalConference = 1;
+        int endTime = conferenceList.get(0)[1];
         for (int i = 1; i < conferenceNum; i++) {
-            int nextStartTime = conferenceSchedule.get(i)[0];
-            if (nextStartTime >= endTime) {
-                totalConferenceNum++;
-                endTime = conferenceSchedule.get(i)[1];
+            int nextStartTime = conferenceList.get(i)[0];
+            if (endTime <= nextStartTime) {
+                totalConference++;
+                endTime = conferenceList.get(i)[1];
             }
         }
-
-        System.out.println(totalConferenceNum);
+        System.out.println(totalConference);
     }
 }
