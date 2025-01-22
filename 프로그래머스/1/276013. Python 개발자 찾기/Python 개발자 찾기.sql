ad527b5a8164d59d251b1a6ef045ac227c2964db
nanayah99 <nanayah99@naver.com>
2025-01-22 16:54:08 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+select
+    ID, EMAIL, FIRST_NAME, LAST_NAME
+from
+    DEVELOPER_INFOS
+where
+    SKILL_1 = 'Python'
+    OR SKILL_2 = 'Python'
+    OR SKILL_3 = 'Python'
+order by
+    ID ASC
