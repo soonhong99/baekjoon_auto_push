235defa274253b7c59bb1d5bd39c0ad9b9143643
nanayah99 <nanayah99@naver.com>
2025-01-24 17:58:41 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,15 +1,13 @@
 -- 코드를 작성해주세요
-select
+select distinct
     D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
 from
     DEVELOPERS D
-join 
-    SKILLCODES S ON (S.CODE & D.SKILL_CODE) = S.CODE
+join
+    SKILLCODES S ON S.CODE & D.SKILL_CODE = S.CODE
 where
-    S.NAME = 'C#'
-    OR S.NAME = 'Python'
-group by
-    D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+    S.NAME = 'Python'
+    OR S.NAME = 'C#'
 order by
     D.ID ASC
     
