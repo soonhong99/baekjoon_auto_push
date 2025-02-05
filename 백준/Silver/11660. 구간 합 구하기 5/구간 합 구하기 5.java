760e4058109272204d45f73601cb06ca56736cb7
nanayah99 <nanayah99@naver.com>
2025-02-06 02:21:30 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1388 ms, Memory: 128664 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -19,51 +19,35 @@ import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class Main {
-    static int[][] list;
-    static int[][] cumulativeList;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        /*
-        첫째 줄에 표의 크기 N과 합을 구해야 하는 횟수 M이 주어진다. (1 ≤ N ≤ 1024, 1 ≤ M ≤ 100,000)
-        둘째 줄부터 N개의 줄에는 표에 채워져 있는 수가 1행부터 차례대로 주어진다.
-        다음 M개의 줄에는 네 개의 정수 x1, y1, x2, y2 가 주어지며, (x1, y1)부터 (x2, y2)의 합을 구해 출력해야 한다.
-        표에 채워져 있는 수는 1,000보다 작거나 같은 자연수이다. (x1 ≤ x2, y1 ≤ y2)
-         */
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int listLength = Integer.parseInt(st.nextToken());
-        int executionNum = Integer.parseInt(st.nextToken());
-        list = new int[listLength + 1][listLength + 1];
-        cumulativeList = new int[listLength + 1][listLength + 1];
-        for (int i = 1; i < listLength + 1; i++) {
-            StringTokenizer row = new StringTokenizer(br.readLine());
-            for (int j = 1; j < listLength + 1; j++) {
-                list[i][j] = Integer.parseInt(row.nextToken());
+        int lengthOfList = Integer.parseInt(st.nextToken());
+        int numberOfSolveProblem = Integer.parseInt(st.nextToken());
+        int[][] list = new int[lengthOfList + 1][lengthOfList + 1];
+
+        for (int i = 1; i < lengthOfList + 1; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            for (int j = 1; j < lengthOfList + 1; j++) {
+                list[i][j] = Integer.parseInt(st2.nextToken());
             }
         }
 
-        /*
-        1 2 3 4 5
-        2 3 4 5 6
-        3 4 5 6 7
-        4 5 6 7 8
-        5 6 7 8 9
-         */
+        int[][] totalAddList = new int[lengthOfList + 1][lengthOfList + 1];
 
-        for (int i = 1; i < listLength + 1; i++) {
-            for (int j = 1; j < listLength + 1; j++) {
-                cumulativeList[i][j] = cumulativeList[i - 1][j] + cumulativeList[i][j - 1] - cumulativeList[i - 1][j - 1] + list[i][j];
+        for (int i = 1; i < lengthOfList + 1; i++) {
+            for (int j = 1; j < lengthOfList + 1; j++) {
+                totalAddList[i][j] = totalAddList[i][j - 1] + totalAddList[i - 1][j] - totalAddList[i - 1][j - 1] + list[i][j];
             }
         }
 
-        for (int i = 0; i < executionNum; i++) {
-            StringTokenizer nums = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(nums.nextToken());
-            int y1 = Integer.parseInt(nums.nextToken());
-            int x2 = Integer.parseInt(nums.nextToken());
-            int y2 = Integer.parseInt(nums.nextToken());
-
-            int totalNum = cumulativeList[x2][y2] - cumulativeList[x1 - 1][y2] - cumulativeList[x2][y1 -1] + cumulativeList[x1- 1][y1 -1];
-            System.out.println(totalNum);
+        for (int i = 0; i < numberOfSolveProblem; i++) {
+            StringTokenizer st3 = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st3.nextToken());
+            int y1 = Integer.parseInt(st3.nextToken());
+            int x2 = Integer.parseInt(st3.nextToken());
+            int y2 = Integer.parseInt(st3.nextToken());
+            System.out.println(totalAddList[x2][y2] - totalAddList[x2][y1 - 1] - totalAddList[x1 - 1][y2] + totalAddList[x1 - 1][y1 - 1]);
         }
     }
 }
